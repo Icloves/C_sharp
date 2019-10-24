52f93879ec6100c930594f5d77eedbce373c9447
Icloves <ilyaoa@gmail.com>
2019-10-24 23:00:13 +0300

    PP

---
+++ Прог 1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите количество элементов");
+            int n = int.Parse(Console.ReadLine());
+            double[] nums = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент", i + 1);
+                nums[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int b = i; b < n; b++)
+                {
+                    
+                    if  ((nums[i] != nums[b]) && ((nums[i]+nums[b]) % 1 == 0))
+                    {
+                        Console.WriteLine(nums[i]);
+                        Console.WriteLine(nums[b]);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        
+    
---
+++ Прог 3/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов");
+            int n = int.Parse(Console.ReadLine());
+
+            string[] nums = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент", i + 1);
+                nums[i] = (Console.ReadLine());
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                string first = nums[i];
+                for (int b = i + 1; b < n; b++)
+                {
+                    string second = nums[b];
+                    bool firstContainsDigitFromSecond = false;
+                    for (int c = 0; c < first.Length; c++)
+                    {
+                        if (second.Contains(first[c]))
+                        {
+                            firstContainsDigitFromSecond = true;
+                            break;
+                        }
+                    }
+                    if (!firstContainsDigitFromSecond)
+                    {
+                        Console.WriteLine(first);
+                        Console.WriteLine(second);
+                    }
+                }
+                Console.ReadLine();
+
+            }
+            
+        }
+        
+    }
+}
+
+
+
+
+
+
